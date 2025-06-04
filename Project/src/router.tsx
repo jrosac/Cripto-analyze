dc521f89c7b3021b93fd9b41de2da8bac894f49c
jrosac <joaorosa.conceicao@dcomp.ufs.br>
2025-06-04 15:33:53 -0300

    feat; added the layout route

--- Project/src/router.tsx
+++ Project/src/router.tsx
@@ -2,9 +2,11 @@ import {createBrowserRouter} from "react-router-dom"
 import { Home } from "./pages/home"
 import { NotFound } from "./pages/notFound"
 import { Detail } from "./pages/detail"
+import { Layout } from "./components/layout"
 
 const router = createBrowserRouter([
     {
+        element: <Layout/>,
         children:[
             {
                 path:"/",
